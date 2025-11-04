ecb24deb73df5b0d9b72c50f82612c3f3833531c
Christopher Whall <chrislwhall@gmail.com>
2025-11-03 16:46:11 -0800

    Datasets and R files added

---
+++ STAT107_FinalProject/00_requirements.R
@@ -0,0 +1,12 @@
+
+if (require(tidyverse)) { ## If package is not installed
+install.packages("tidyverse") ## Install
+library(tidyverse) ## Load package to environment.
+}
+if (require(readxl)) { ## If package is not installed
+  install.packages("readxl") ## Install
+  library(readxl) ## Load package to environment.
+}
+
+
+
