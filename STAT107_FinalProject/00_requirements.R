51757ebec5e92ea97b4a08000fb78aa31130114f
joil004 <jonthemeh@gmail.com>
2025-11-04 17:40:07 -0800

    Missing Values + more
    Checked for missing values. Removed the factor part as its unnecessary. Fixed if (require) to if(!require) in requirements script.

--- STAT107_FinalProject/00_requirements.R
+++ STAT107_FinalProject/00_requirements.R
@@ -1,11 +1,12 @@
 
-if (require(tidyverse)) { ## If package is not installed
-install.packages("tidyverse") ## Install
-library(tidyverse) ## Load package to environment.
+if (!require(tidyverse)) {
+  install.packages("tidyverse")
+  library("tidyverse")
 }
-if (require(readxl)) { ## If package is not installed
-  install.packages("readxl") ## Install
-  library(readxl) ## Load package to environment.
+
+if (!require(readxl)) { 
+  install.packages("readxl") 
+  library(readxl)
 }
 
 
