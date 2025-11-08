5d72d5c14136d4809ae913dfc05ec87eae3326f8
Christopher Whall <chrislwhall@gmail.com>
2025-11-07 16:26:14 -0800

    Added knitr to requirements

--- STAT107_FinalProject/00_requirements.R
+++ STAT107_FinalProject/00_requirements.R
@@ -1,13 +1,15 @@
 
 if (!require(tidyverse)) {
   install.packages("tidyverse")
-  library("tidyverse")
 }
+library("tidyverse")
 
 if (!require(readxl)) { 
   install.packages("readxl") 
-  library(readxl)
 }
+library(readxl)
 
-
-
+if (!require(knitr)) { 
+  install.packages("knitr") 
+}
+library(knitr)
